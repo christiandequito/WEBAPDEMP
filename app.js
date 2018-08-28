503d5b99d7bd9f411fb37e3367cc7b961ba2dcf4
christiandequito <40710060+christiandequito@users.noreply.github.com>
2018-08-28 19:27:46 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+/*******************IMPORTS**********************/
+
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const bodyparser = require("body-parser");
+const cookieparser = require("cookie-parser");
+const hbs = require("hbs");
+const path = require("path")
+
+
+/*******************  SETUP  **********************/
+const app = express();
+const port = 3000;
+
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://marvic379:wew789033541@ds135852.mlab.com:35852/tameme-o",{
+    useNewUrlParser: true
+})
+app.set("view-engine", "hbs")
+app.use(express.static(__dirname + "/public"))
+
+app.use(cookieparser())
+
+app.use(session({
+  secret : "secret",
+  name : "secretname",
+  resave: true,
+  saveUninitialized :true,
+    
+  cookie: {
+    maxAge: 21*24*60*60*1000 //3 weeks
+  }
+}))
+
+app.use(require("./controller"))
+
+app.listen(process.env.PORT || port, () => {
+    console.log("Listening in port " + port);
+});
